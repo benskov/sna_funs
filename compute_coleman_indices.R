f4e4ce2043635078eb7069956013b7afecd97ca2
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2016-12-19 18:19:19 +0100

    Faster version

--- compute_coleman_indices.R
+++ compute_coleman_indices.R
@@ -2,60 +2,57 @@
 # by Bojanowski and Corten (2014). It uses each ego's local network as the sub-graph to 
 # compute ego's segregation index.
 
-coleman_index <- function (G, g) { 
-  # G is the network object, g is a factor with group assignments
-  
-  # Safety move
-    stopifnot(is.network(G), is.factor(g)) # Making sure G has correct class, and g is a factor
+segregation <- function (G, g, index = "coleman") { 
+    # G is the network object, g is a vector with group assignments (coerced to factor)
     
-  # Housekeeping
-    diag.remove(G, remove.val = 1) # Set diagonals to 1 (this is taken into account when computing out degrees below)
-    vertices <- network.vertex.names(G)
-    g <- as.factor(g) # Coerce g into factor
-    N <- G$gal$n # Network size
-    A <- as.sociomatrix.sna(G, force.bipartite = TRUE) # Adjacency matrix, which we use repeatedly in loop
-    out_degs <- rowSums(A) - 1 # 1-by-N vector with out-degrees; 1 is subtracted to counter 1-diagonals
-    n <- table(g) # Look-up table with frequencies of each group (used in for loop)
-  
-  # Create "group-weight" matrix with information on same-group nominations
+    # Safety moves
+        stopifnot(!is.null(g), index == "coleman") # only Coleman index supported so far
+        
+    # Housekeeping
+        if (is.network(G)) {
+            A <- as.sociomatrix.sna(G, force.bipartite = TRUE)
+            vertices <- network.vertex.names(G)
+            N <- network.size(G)
+        } else if (is.matrix(G) & !is.null(rownames(G)) & all(rownames(G) == colnames(G))) {
+            A <- G
+            vertices <- colnames(G)
+            N <- nrow(G)
+        } else {
+            stop("Please, provide valid arguments.")
+        }
+        diag(A) <- 1 # Set diagonals to 1 (this is taken into account when computing out degrees below)
+        out_degs <- rowSums(A) - 1 # 1-by-N vector with out-degrees; 1 is subtracted to counter 1-diagonals
+        g <- if (is.null(names(g))) setNames(as.factor(g), vertices) else as.factor(g) # Coerce g into factor
+        n <- table(g) # Look-up table with frequencies of each group (used in for loop)
+    
+    # Create "group-weight" matrix with information on same-group nominations
     W_group <- matrix(0, nrow = N, ncol = N) # An empty matrix we populate below
     for (i in levels(g)) {
-      same_group <- which(i == g) # Find all nodes in i'th group
-      W_group[same_group, same_group] <- 1 # w_ij set to 1 when i and j are in the same group
+        same_group <- which(i == g) # Find all nodes in i'th group
+        W_group[same_group, same_group] <- 1 # w_ij set to 1 when i and j are in the same group
     }
     # A weakness of this method is that it's black-and-white, i.e., either you're in group i
     # or you're not. But groups i and j may be more alike than i and k, and there should
     # be a way to reflect this. It's especially true if the grouping variable is actually
     # continuous, so not grouping per se.
-  
-  # Same-group adjacency matrix by element-wise multiplication
+    
+    # Same-group adjacency matrix by element-wise multiplication
     A_group <- A * W_group 
-  
-  # Generating the data for output
+    
+    # Generating the data for output
     o <- data.frame(vertex = vertices, m_exp = NA_integer_, m = NA_integer_)
     subgraphs <- list()
     for (v in vertices) {
-      subgraphs[[v]] <- unname(A[rownames(A) == v, ] == 1) # Sub-graph is i and i's alters; unnamed so subsetting possible (below)
-      subgraph <- subgraphs[[v]]
-      o[o$vertex == v, "m_exp"] <- sum(out_degs[subgraph]) * (n[g[i]] - 1)/(N - 1) # i's expected number of same-group nominations 
-      o[o$vertex == v, "m"] <- sum(A_group[subgraph, subgraph]) # i's actual number of same-group nominations
+        subgraphs[[v]] <- unname(A[rownames(A) == v, ] == 1) # Sub-graph of i and i's alters; unnamed so subsetting possible (below)
+        subgraph <- subgraphs[[v]]
+        vert <- which(o$vertex == v)
+        o[vert, "m_exp"] <- sum(out_degs[subgraph]) * (n[g[v]] - 1)/(N - 1) # i's expected number of same-group nominations 
+        o[vert, "m"] <- sum(A_group[subgraph, subgraph]) # i's actual number of same-group nominations
     }
     o$score <- ifelse(o$m >= o$m_exp, 
-                      (o$m - o$m_exp)/(sum(out_degs[subgraphs[o$vertex]]) - o$m_exp,
+                      (o$m - o$m_exp)/(sum(out_degs[subgraph]) - o$m_exp),
                       (o$m - o$m_exp)/o$m_exp)
-    o$group <- g
-    o[, -c("m_exp", "m") # Returns relevant columns of data framwe
+    o$group <- g[vertices]
+    o[, -c(2, 3)] # Removes irrelevant columns from output
 }
 
-timetest <- data.frame(N = 0, user.self = 0, sys.self = 0, elapsed = 0)
-for(N in seq(10, 3000, length.out = 15)) {
-  N <- round(N)
-  G <- network(rgraph(N)$adj_matrix, directed = TRUE, matrix.type = "adjacency") 
-  g <- rep(LETTERS[1:5], N/5+10)[1:N]
-  timetest <- rbind(timetest, c(N, system.time(coleman_index(G, g))[1:3]))
-}; timetest[-1, ]; plot(timetest$elapsed[-1])
-
-coleman_index() %>% ggplot(aes(x = coleman_segregation_index, fill = group)) + 
-  geom_histogram(binwidth = 0.1) +
-  facet_wrap(~ group, ncol = 1) + 
-  scale_x_continuous(limits = c(-1.1, 1.1))
