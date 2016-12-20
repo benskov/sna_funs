cf8b838d283327435b141e1890d735e3fda857bb
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2016-12-20 14:13:20 +0100

    Fine-tuning and adding documentation
    Need to fix the documentation so compute_int_rad_scores() has its own manual, explaining its internal nature - and one manual for both integration() and radiality() with proper description and examples.

--- compute_integration_radiality_scores.R
+++ compute_integration_radiality_scores.R
@@ -1,45 +1,64 @@
+# DOCUMENTATION NOT PROPERLY FORMATTED TO ACCOUNT FOR INTEGRATION() AND RADIALITY() INVOKING THE ACTUAL FUNCTION. NEEDS TO BE FIXED.
+
+#' Compute integration and radiality scores
+#' 
+#' Implements the integration and radiality scores proposed by Valente and 
+#' Foreman (1998). Integration is a global measure of inward centrality, 
+#' radiality of outward centrality.
+#' 
+#' These functions draw heavily on \code{sna::geodist()} and may be slow for 
+#' large, dense networks.
+#' 
+#' @param score length-one character indicating which score to compute.
+#' @param G a \code{network} object of matrix coericble into one.
+#' @param rm.autonom logical; should autonomiations be ignored when computing
+#'   the scores?
+#'   
+#' @return 
+#' A tidy data frame with four columns: node names, absolute scores,
+#' relative scores, and reversed scores (as percentages).
+#' 
+#' @examples 
+#' nodes <- LETTERS[1:20] # Node names
+#' A <- matrix(sample(0:1, 400, TRUE, c(0.75, 0.25)), 20, 20, dimnames = list(nodes, nodes))
+#' integration(A)
+#' radiality(A)
+
 integration <- function(...) {
-  compute_int_rad_scores("i", ...)
+    compute_int_rad_scores("i", ...)
 }
 
 radiality <- function(...) {
-  compute_int_rad_scores("r", ...)
+    compute_int_rad_scores("r", ...)
 }
 
 compute_int_rad_scores <- function (score = "i", G, rm.autonom = TRUE) { 
-  # Internal function; used by integration() and radiality()
-  
-  # Implements integration and radiality scores, as proposed by Valente and Foreman (1998) in 
-  # Social Networks 20 (1998); 89-105
-  
-  stopifnot(is.network(G)) 
-  if (rm.autonom) sna::diag.remove(G, 0) # Removing autonominations
-  
-  geodesics <- sna::geodist(G, inf.replace = 0)$gdist # max(geodesics) fails if inf.replace != 0
-  max_geodesic <- max(geodesics)
-  reversed_distances <- ifelse(geodesics == 0, 0, (1 + max_geodesic) - geodesics)
-    # Generates matrix of reversed distances, in which:
-    # - Vertices have RD = 0 to themselves, and
-    # - Isolates have RD = 0 to all other vertices
-  max_reversed_distance <- max(reversed_distances)
-  scores <- if (score == "r") { # Radiality score
-    rowSums(reversed_distances)/(G$gal$n - 1) 
-  } else if (score == "i") { # Integration scores (default)
-    colSums(reversed_distances)/(G$gal$n - 1)
-  } else {
-    stop("Incorrect score type; must be either integration (i) or radiality (r).")
-  } # G$gal$n = number of vertices
-  
-  # Build and return output as tidy data frame:
-  o <- data.frame(ego = network.vertex.names(G), absolute = scores)
-  o$relative <- o$absolute/max_reversed_distance
-  o$reversed <- (1 - o$relative) * 100 # As percentages
-  o
+    # Internal function; invoked by integration() and radiality()
+    
+    # Savety moves and housekeeping
+    if (!is.matrix(G) & !network::is.network(G)) stop("The function requires a network object or a matrix coercible into one.")
+    G <- network::as.network(G)
+    if (rm.autonom) sna::diag.remove(G, 0)
+    N <- network::network.size(G)
+    
+    geodesics <- sna::geodist(G, inf.replace = 0)$gdist # max(geodesics) meaningless if inf.replace != 0
+    max_geodesic <- max(geodesics)
+    reversed_distances <- ifelse(geodesics == 0, 0, (1 + max_geodesic) - geodesics)
+        # Generates matrix of reversed distances, in which:
+        # - Vertices have RD = 0 to themselves, and
+        # - Isolates have RD = 0 to all other vertices
+    max_reversed_distance <- max(reversed_distances)
+    scores <- if (score == "i") { # Integration scores (default)
+        colSums(reversed_distances)/(N - 1)
+    } else if (score == "r") { # Radiality score
+        rowSums(reversed_distances)/(N - 1) 
+    } else {
+        stop("Incorrect type of score; must be either integration (i) or radiality (r). Please, use the dedicated functions integration() or radiality() to avoid this problem.")
+    }
+    
+    # Build and return output as tidy data frame:
+    o <- data.frame(node = network.vertex.names(G), absolute = scores)
+    o$relative <- o$absolute/max_reversed_distance
+    o$reversed <- (1 - o$relative) * 100 # As percentages
+    o
 }
-
-timetest <- data.frame(N = 0, user.self = 0, sys.self = 0, elapsed = 0)
-for(N in seq(10, 3000, length.out = 15)) {
-  N <- round(N)
-  G <- network(rgraph(N)$adj_matrix, directed = TRUE, matrix.type = "adjacency") 
-  timetest <- rbind(timetest, c(N, system.time(integration(G))[1:3]))
-}; timetest[-1, ]; plot(timetest$elapsed[-1], log = "xy")
